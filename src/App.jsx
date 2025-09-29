2f8df2da4932fe848e06de68a7f137dd90b54fed
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2025-09-29 23:34:03 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="max-w-7xl mx-auto py-8">
+      <div className="grid grid-cols-3 ">
+        <div className="col-span-2">product category</div>
+        <div className="">product adding</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
